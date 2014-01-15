54819b49fa7477197f58df14058bab9107f9dcac
Kazuyuki Honda <hakobera@gmail.com>
2014-01-15 17:53:08 +0900

    Rewrite test with co

--- test/aws.js
+++ test/aws.js
@@ -1,240 +1,236 @@
 var aws = require('../lib/aws');
 var should = require('should');
 var sinon = require('sinon');
+var co = require('co');
 var uuid = require('node-uuid');
 var AWS = require('aws-sdk');
 var dynamodb = new AWS.DynamoDB();
 var s3 = new AWS.S3();
 
+var getItem = function (params) {
+  return function (fn) { dynamodb.getItem(params, fn); }
+};
+
+var putItem = function (params) {
+  return function (fn) { dynamodb.putItem(params, fn); }
+};
+
+var getSignedUrl = function (method, params) {
+  return function (fn) { s3.getSignedUrl(method, params, fn); }
+};
+
+var headObject = function (params) {
+  return function (fn) { s3.headObject(params, fn); }
+};
+
+var putObject = function (params) {
+  return function (fn) { s3.putObject(params, fn); }
+};
+
+var getObject = function (params) {
+  return function (fn) { s3.getObject(params, fn); }
+}
+
 describe('aws', function () {
-  this.timeout(5000);
+  this.timeout(10000);
 
   describe('#putItem', function () {
     it('should put new item', function (done) {
-      var id = uuid.v1();
-      aws.putItem({ id: id, key1: 'value' })(function (err, res) {
-        if (err) return done(err);
+      co(function* () {
+        var id = uuid.v1();
+        var item = { id: id, key1: 'value' };
 
-        dynamodb.getItem({
+        yield aws.putItem(item);
+
+        var data = yield getItem({
           TableName: process.env.AWS_DYNAMODB_TABLE,
           Key: { id: { "S": id } }
-        }, function (err, data) {
-          if (err) return done(err);
-          data.Item.id.S.should.equal(id);
-          data.Item.key1.S.should.equal('value');
-          done();
         });
-      });
+
+        data.Item.id.S.should.equal(item.id);
+        data.Item.key1.S.should.equal(item.key1);
+      })(done);
     });
 
     it('should override item when id is already exists', function (done) {
-      var id = uuid.v1();
-      aws.putItem({ id: id, key1: 'value' })(function (err, res) {
-        if (err) return done(err);
+      co(function* () {
+        var id = uuid.v1();
+        var item = { id: id, key1: 'value' };
+        var newItem = { id: id, key1: 'value1', key2: 'value2' };
 
-        aws.putItem({ id: id, key1: 'value1', key2: 'value2' })(function (err, res) {
-          if (err) return done(err);
+        yield aws.putItem(item);
+        yield aws.putItem(newItem);
 
-          dynamodb.getItem({
-            TableName: process.env.AWS_DYNAMODB_TABLE,
-            Key: { id: { "S": id } }
-          }, function (err, data) {
-            if (err) return done(err);
-            data.Item.id.S.should.equal(id);
-            data.Item.key1.S.should.equal('value1');
-            data.Item.key2.S.should.equal('value2');
-            done();
-          });
+        var data = yield getItem({
+          TableName: process.env.AWS_DYNAMODB_TABLE,
+          Key: { id: { "S": id } }
         });
-      });
+
+        data.Item.id.S.should.equal(newItem.id);
+        data.Item.key1.S.should.equal(newItem.key1);
+        data.Item.key2.S.should.equal(newItem.key2);
+      })(done);
     });
 
     it('should throw error when data.id is empty', function (done) {
-      var id = uuid.v1();
-      aws.putItem({ key: 'value' })(function (err, data) {
-        if (err && err.message === 'One or more parameter values were invalid: Missing the key id in the item') {
-          return done();
+      co(function* () {
+        var id = uuid.v1();
+        var item = { key: 'value' };
+
+        try {
+          yield aws.putItem(item);
+        } catch (err) {
+          if (err && err.message === 'One or more parameter values were invalid: Missing the key id in the item') {
+            return;
+          }
+          throw err;
         }
-        if (err) return done(err);
-        done(new Error('ValidationException should throw.'));
-      });
+      })(done);
     });
   });
 
   describe('#getItem', function () {
     it('should return item specified by id', function (done) {
-      var id = uuid.v1();
-      dynamodb.putItem({
-        TableName: process.env.AWS_DYNAMODB_TABLE,
-        Item: { id: { "S": id }, key1: { "S": "value" } }
-      }, function (err, res) {
-        aws.getItem(id)(function (err, data) {
-          if (err) return done(err);
-          data.id.should.equal(id);
-          data.key1.should.equal('value');
-          done();
+      co(function* () {
+        var id = uuid.v1();
+        yield putItem({
+          TableName: process.env.AWS_DYNAMODB_TABLE,
+          Item: { id: { "S": id }, key1: { "S": "value" } }
         });
-      });
+
+        var data = yield aws.getItem(id);
+        data.id.should.equal(id);
+        data.key1.should.equal('value');
+      })(done);
     });
 
     it('should return empty data if id is not exists', function (done) {
-      var id = 'invalid';
-      aws.getItem(id)(function (err, data) {
-        if (err) return done(err);
+      co(function* () {
+        var id = 'invalid';
+        var data = yield aws.getItem(id);
         should.not.exists(data.id);
         Object.keys(data).should.have.length(0);
-        done();
-      });
+      })(done);
     });
   });
 
   describe('#signedUploadUrl', function () {
     it('should return pre-signed putObject URL expired 60 seconds', function (done) {
-      var opts = {
-        id: 'test'
-      };
-
-      aws.signedUploadUrl(opts)(function (err, url) {
-        if (err) return done(err);
-
-        s3.getSignedUrl('putObject', {
+      co(function* () {
+        var opts = { id: 'test' };
+        var url = yield aws.signedUploadUrl(opts);
+        var expected = yield getSignedUrl('putObject', {
           Bucket: process.env.AWS_S3_BUCKET,
           Key: opts.id,
           Expires: 60,
           ContentType: 'binary/octet-stream'
-        }, function (err, expected) {
-          if (err) return done(err);
-          url.should.equal(expected);
-          done();
         });
-      });
+        url.should.equal(expected);
+      })(done);
     });
 
     it('should return pre-signed putObject URL expired specified by arguments', function (done) {
-      var opts = {
-        id: 'test',
-        expires: 120
-      };
-
-      aws.signedUploadUrl(opts)(function (err, url) {
-        if (err) return done(err);
-
-        s3.getSignedUrl('putObject', {
+      co(function* () {
+        var opts = {
+          id: 'test',
+          expires: 120
+        };
+        var url = yield aws.signedUploadUrl(opts);
+        var expected = yield getSignedUrl('putObject', {
           Bucket: process.env.AWS_S3_BUCKET,
           Key: opts.id,
           Expires: opts.expires,
           ContentType: 'binary/octet-stream'
-        }, function (err, expected) {
-          if (err) return done(err);
-          url.should.equal(expected);
-          done();
         });
-      });
+        url.should.equal(expected);
+      })(done);
     });
   });
 
   describe('#signedDownloadUrl', function () {
     it('should return pre-signed getObject URL expired 30 seconds', function (done) {
-      var opts = {
-        id: 'test'
-      };
-
-      aws.signedDownloadUrl(opts)(function (err, url) {
-        if (err) return done(err);
-
-        s3.getSignedUrl('getObject', {
+      co(function* () {
+        var opts = {
+          id: 'test'
+        };
+        var url = yield aws.signedDownloadUrl(opts);
+        var expected = yield getSignedUrl('getObject', {
           Bucket: process.env.AWS_S3_BUCKET,
           Key: opts.id,
           Expires: 30
-        }, function (err, expected) {
-          if (err) return done(err);
-          url.should.equal(expected);
-          done();
         });
-      });
+        url.should.equal(expected);
+      })(done);
     });
 
     it('should return pre-signed getObject URL expired specified by arguments', function (done) {
-      var opts = {
-        id: 'test',
-        expires: 120
-      };
-
-      aws.signedDownloadUrl(opts)(function (err, url) {
-        if (err) return done(err);
-
-        s3.getSignedUrl('getObject', {
+      co(function* () {
+        var opts = {
+          id: 'test',
+          expires: 120
+        };
+        var url = yield aws.signedDownloadUrl(opts);
+        var expected = yield getSignedUrl('getObject', {
           Bucket: process.env.AWS_S3_BUCKET,
           Key: opts.id,
           Expires: opts.expires
-        }, function (err, expected) {
-          if (err) return done(err2);
-          url.should.equal(expected);
-          done();
         });
-      });
+        url.should.equal(expected);
+      })(done);
     });
   });
 
   describe('#existsObject', function () {
     it('should return true if object exists in bucket', function (done) {
-      var id = uuid.v1();
-      s3.putObject({
-        Bucket: process.env.AWS_S3_BUCKET,
-        Key: id,
-        ContentType: 'binary/octet-stream',
-        Body: 'test'
-      }, function (err, res) {
-        if (err) return done(err);
-
-        aws.existsObject(id)(function (err, exist) {
-          if (err) return done(err);
-          exist.should.be.true;
-          done();
+      co(function *() {
+        var id = uuid.v1();
+        yield putObject({
+          Bucket: process.env.AWS_S3_BUCKET,
+          Key: id,
+          ContentType: 'binary/octet-stream',
+          Body: 'test'
         });
-      });
+        var exist = yield aws.existsObject(id);
+        exist.should.be.true;
+      })(done);
     });
 
     it('should return false if object does not exists in bucket', function (done) {
-      aws.existsObject('invalid')(function (err, exist) {
-        if (err) return done(err);
+      co(function* () {
+        var exist = yield aws.existsObject('invalid');
         exist.should.be.false;
-        done();
-      });
+      })(done);
     });
   });
 
   describe('#copyObject', function () {
     it('should only overwrite "ContentType" and "ContentDisposition" of current object', function (done) {
-      var id = uuid.v1();
-      s3.putObject({
-        Bucket: process.env.AWS_S3_BUCKET,
-        Key: id,
-        ContentType: 'text/plain',
-        Body: 'test'
-      }, function (err, res) {
-        if (err) return done(err);
+      co(function* () {
+        var id = uuid.v1();
+
+        yield putObject({
+          Bucket: process.env.AWS_S3_BUCKET,
+          Key: id,
+          ContentType: 'text/plain',
+          Body: 'test'
+        });
 
         var item = {
           id: id,
           filename: 'test.txt'
         };
-        aws.copyObject(item)(function (err, res) {
-          if (err) return done(err);
 
-          s3.getObject({
-            Bucket: process.env.AWS_S3_BUCKET,
-            Key: id
-          }, function (err, data) {
-            if (err) return done(err);
+        yield aws.copyObject(item);
 
-            data.ContentType.should.equal('binary/octet-stream');
-            data.ContentDisposition.should.equal("attachment; filename*=UTF-8''test.txt");
-            data.Body.toString().should.equal('test');
-            done();
-          });
+        var data = yield getObject({
+          Bucket: process.env.AWS_S3_BUCKET,
+          Key: id
         });
-      });
+
+        data.ContentType.should.equal('binary/octet-stream');
+        data.ContentDisposition.should.equal("attachment; filename*=UTF-8''test.txt");
+        data.Body.toString().should.equal('test');
+      })(done);
     });
   });
 
--- test/render.js
+++ test/render.js
@@ -1,21 +1,24 @@
-var should = require('should');
 var render = require('../lib/render');
+var co = require('co');
 
 describe('render', function () {
   it('should return html using swig template', function (done) {
-    render('test', { key: 'value' })(function (err, data) {
-      if (err) return done(err);
+    co(function* () {
+      var data = yield render('test', { key: 'value' });
       data.should.eql('<div>value</div>\n');
-      done();
-    });
+    })(done);
   });
 
   it('should throw error when template not found', function (done) {
-    render('invalid')(function (err, data) {
-      if (err && err.code == 'ENOENT') {
-        return done();
+    co(function* () {
+      try {
+        yield render('invalid');
+      } catch (err) {
+        if (err && err.code == 'ENOENT') {
+          return;
+        }
+        throw err;
       }
-      done(new Error("Error with code 'ENOENT' should be thrown."));
-    });
+    })(done);
   });
 });
