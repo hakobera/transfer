5bf0fd043089ee496d45af04354209a7b2872ecb
Kazuyuki Honda <hakobera@gmail.com>
2014-01-15 11:59:36 +0900

    Add test of aws

---
+++ test/aws.js
@@ -0,0 +1,29 @@
+var aws = require('../lib/aws');
+var should = require('should');
+var sinon = require('sinon');
+var uuid = require('node-uuid');
+
+describe('aws', function () {
+
+  describe('#putItem', function () {
+    it('should store specified data', function (done) {
+      var id = uuid.v1();
+      aws.putItem({ id: id })(function (err, data) {
+        if (err) return done(err);
+        done(err);
+      });
+    });
+
+    it('should throw error when data.id is empty', function (done) {
+      var id = uuid.v1();
+      aws.putItem({ key: 'value' })(function (err, data) {
+        if (err && err.message === 'One or more parameter values were invalid: Missing the key id in the item') {
+          return done();
+        }
+        if (err) return done(err);
+        done(new Error('ValidationException should throw.'));
+      });
+    });
+  });
+
+});
